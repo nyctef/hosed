d6e95065c476c65687939a9bade15c816a4bbb2f
Nyctef <nyctef@nyctef.com>
2014-12-24 15:25:37 +0000

    ignore deprecation warnings
    the only one we have at the moment is caused by rails-api not us

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
+::ActiveSupport::Deprecation.silenced = true
 Rails.application.initialize!
