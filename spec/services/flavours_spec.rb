cebd7c97625ed6abe69d5687edaa30daa810ec64
Nyctef <nyctef@nyctef.com>
2014-12-24 17:24:46 +0000

    extract a couple lets

--- spec/services/flavours_spec.rb
+++ spec/services/flavours_spec.rb
@@ -2,23 +2,25 @@ describe Flavours do
   before :each do
     Flavours.reset
   end
+
+  let (:url) { "a-url" }
+  let (:drop) { Drop.new(url: url) }
+
   it "accepts a drop to process" do
-    Flavours.start(Drop.new(url: "a-url"))
+    Flavours.start(drop)
   end
   it "accepts a flavour to add" do
     Flavours.add { |url| {foo: "bar"} }
   end
   it "runs flavours on processed urls" do
     fl = double("flavour")
-    expect(fl).to receive(:call).with("a-url").and_return({foo: "bar"})
-    drop = Drop.new(url: "a-url")
+    expect(fl).to receive(:call).with(url).and_return({foo: "bar"})
 
     Flavours.add fl
     Flavours.start(drop)
   end
   it "adds returned key/value pairs to the model" do
     Flavours.add { |url| {baz: "qux"} }
-    drop = Drop.new(url: "a-url")
     expect(drop).to receive(:set).with(:baz => "qux")
 
     Flavours.start(drop)
