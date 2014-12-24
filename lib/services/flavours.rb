2cd0d1a69b8a0e57b2cf5e2841e8eaa86da59747
Nyctef <nyctef@nyctef.com>
2014-12-24 17:21:30 +0000

    implement synchronous Flavours with tests

--- lib/services/flavours.rb
+++ lib/services/flavours.rb
@@ -1,4 +1,23 @@
 class Flavours
+  @flavours = []
+
+  def self.reset
+    @flavours = []
+  end
+
   def self.start(drop)
+    @flavours.each { |f| 
+      attrs = f.call(drop.url)
+      attrs.each { |k,v|
+        drop.set(k => v)
+      }
+    }
+  end
+
+  def self.add(callable=nil, &block)
+    if not callable.present? ^ block.present?
+      raise "exactly one callable or block is required"
+    end
+    @flavours << (callable || block)
   end
 end
--- spec/services/flavours_spec.rb
+++ spec/services/flavours_spec.rb
@@ -1,2 +1,26 @@
 describe Flavours do
+  before :each do
+    Flavours.reset
+  end
+  it "accepts a drop to process" do
+    Flavours.start(Drop.new(url: "a-url"))
+  end
+  it "accepts a flavour to add" do
+    Flavours.add { |url| {foo: "bar"} }
+  end
+  it "runs flavours on processed urls" do
+    fl = double("flavour")
+    expect(fl).to receive(:call).with("a-url").and_return({foo: "bar"})
+    drop = Drop.new(url: "a-url")
+
+    Flavours.add fl
+    Flavours.start(drop)
+  end
+  it "adds returned key/value pairs to the model" do
+    Flavours.add { |url| {baz: "qux"} }
+    drop = Drop.new(url: "a-url")
+    expect(drop).to receive(:set).with(:baz => "qux")
+
+    Flavours.start(drop)
+  end
 end
