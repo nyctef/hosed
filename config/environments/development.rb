b4ad02f70f08c342b125d55efc5b39844a6a5ddf
Nyctef <nyctef@nyctef.com>
2014-12-25 01:59:21 +0000

    set log level appropriately for dev/test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.log_level = :info
+  config.logger.level = Logger::Info
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
